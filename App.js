255d26ce97f837213a81fd4071fa3a642fce88af
ivanlandinez1994 <ivan.landinez.gallego@gmail.com>
2020-07-11 16:24:46 -0300

    Axios post

--- App.js
+++ App.js
@@ -3,10 +3,11 @@ import { AsyncStorage, Text, Button,TextInput, View, StyleSheet} from 'react-nat
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 
+
 import Maiin from "./src/screens/main"
 import NavegaLog from './src/navigation/navegacionLog'
-const AppContext = React.useContext(); 
 
+const AuthContext = React.createContext(); // creando el contexto
 
 function SplashScreen() {
   return (
@@ -21,16 +22,43 @@ function HomeScreen() {
 
   return (
     <Maiin salida={signOut}/>
+
+
   );
 }
 
 function SignInScreen() {
-  return (    
+ 
+  const [username, setUsername] = React.useState('');
+  const [password, setPassword] = React.useState('');
+
+  const { signIn } = React.useContext(AuthContext);
+
+  return (
+    
+    /*<View>
+      <TextInput
+        placeholder="Username"
+        value={username}
+        onChangeText={setUsername}
+      />
+      <TextInput
+        placeholder="Password"
+        value={password}
+        onChangeText={setPassword}
+        secureTextEntry
+      />
+      <Button title="Sign in" onPress={() => signIn({ username, password })} />
+    </View>
+    */
+    
     <NavegaLog   />
+  
   );
 }
 
 const Stack = createStackNavigator();
+
 
 export default function App({ navigation }) {
   const [state, dispatch] = React.useReducer(
@@ -40,34 +68,86 @@ export default function App({ navigation }) {
           return {
             ...prevState,
             userToken: action.token,
-            isLoading: false
+            isLoading: false,
           };
         case 'SIGN_IN':
           return {
             ...prevState,
-            isSignout: false, 
-            userToken: action.token 
+            isSignout: false,
+            userToken: action.token,
           };
         case 'SIGN_OUT':
           return {
             ...prevState,
             isSignout: true,
-            userToken: null
+            userToken: null,
           };
       }
     },
     {
-      isLoading: true,  // isLoading: se setea a true cuando se trata de chequear si hay un token ya almacenado en el asyncStorage
-      isSignout: false, //isSignOut: se setea a true cuando el usuario cierre sesion
-      userToken: null, // userToken: es el token del usuario, si no es nulo (null) se asume que el usuario está logueado, sino NO.
+      isLoading: true,
+      isSignout: false,
+      userToken: null,
     }
+  );
+
+// AsyncStorage es una fachada que permite persistir datos en forma offline
+
+/*Autentificación basada en token
+En este método, el usuario se identifica al igual que con la autenticación básica, con sus credenciales,
+ nombre de usuario y contraseña. Pero en este caso, con la primera petición de autentificación, el servidor 
+ generará un token basado en esas credenciales.
+El servidor guarda en base de datos este registro y lo devuelve al usuario para que a partir de ese momento no 
+envíe más credenciales de inicio de sesión en cada petición HTTP.  En lugar de las credenciales, simplemente se 
+debe enviar el token codificado en cada petición HTTP.*/
+
+  React.useEffect(() => {
+    // Toma el token almacenado en el  storage
+    const bootstrapAsync = async () => {
+      let userToken;
+
+      try {
+        userToken = await AsyncStorage.getItem('userToken');
+      } catch (e) {
+        // en caso de que el restoring del token falle
+      }
+
+      // Despues de obtener el token, habria que validarlo 
+
+      // This will switch to the App screen or Auth screen and this loading
+      // screen will be unmounted and thrown away.
+      dispatch({ type: 'RESTORE_TOKEN', token: userToken });
+    };
+
+    bootstrapAsync();
+  }, []);
+
+  const authContext = React.useMemo(
+    () => ({
+      signIn: async data => {
+        //En la app de produccion, se necesita mandar el usuario y contraseña al servidor para obtener el token
+        // tambien se necesita manejar errores en caso de que el inicio de sesion falle
+        // despues de obtener el token se necesita persistirlo usando `AsyncStorage`
+        // De momento el token está harcodeado para que funcione el flow de autenticacion
+
+        dispatch({ type: 'SIGN_IN', token: 'dummy-auth-token' });
+      },
+      signOut: () => dispatch({ type: 'SIGN_OUT' }),
+      
+      signUp: async data => {
+        
+        //pasa lo mismo que con el signIn
+
+        dispatch({ type: 'SIGN_IN', token: 'dummy-auth-token' });
+      },
+    }),
+    []
   );
 
   return (
     //el provider del contexto en su propiedad value tiene el valor de los datos a compartir
-    <AppContext.Consumer>
-      {value =>{
-        <NavigationContainer >
+    <AuthContext.Provider value={authContext}>
+      <NavigationContainer >
         <Stack.Navigator screenOptions={{headerShown: false,}}>
           {state.isLoading ? (
             // Mientras se revisa el token, aparece un splash screen
@@ -89,24 +169,21 @@ export default function App({ navigation }) {
           )}
         </Stack.Navigator>
       </NavigationContainer>
-      }}
-    </AppContext.Consumer>
+    </AuthContext.Provider>
   );
 }
-
-
-
 
 const styles = StyleSheet.create({
   
   container: {
-    flex:1,    
+    //paddingTop:26,
+    flex:1,
+    //backgroundColor:'#dfe1e6',
+    
    },
    container1: {
+    //paddingTop:26,
     flex: 0.1,
    }
   
 });
-
-
-
--- src/screens/login.js
+++ src/screens/login.js
@@ -7,21 +7,18 @@ import { Formik } from 'formik';
 import * as yup from 'yup';
 const axios = require('axios');
 
-import Titulo from '../components/titulo';
 
-const AppContext = React.createContext({
-  token: '',
-  userName: ''
-}); // creando el contexto
+import Titulo from '../components/titulo';
 
 const validationSchema = yup.object().shape({
   userName: yup
     .string()
-    .label('userName')
+    .label('usuario')
+    //.email()
     .required(),
   contrasenia: yup
     .string()
-    .label('contrasenia')
+    .label('Contraseña')
     .required()
     .min(2, 'Seems a bit short...')
     .max(10, 'We prefer insecure system, try a shorter password.'),
@@ -31,97 +28,92 @@ export default class Login extends React.Component {
   
   render(){
     return(
-      <View style={styles.container}>
-        
-        <ImageBackground 
-          source={require('../assets/blured.jpg')}
-          style={{ flex: 1,
-            width: null,
-            height: null,
-            }}>
-            <AppContext.Provider value={AppContext}>
-              <View style={styles.container2}>
-                <Titulo content='Tripo' /> 
-                <Text style={{ marginLeft: 30,fontSize:24,color:"#2a3078" }}>Login</Text>
-                <SafeAreaView style={{ marginTop: 40 }}>
-                  <Formik
-                    initialValues={{ userName: '', contrasenia: '' }}
-                    onSubmit={(values, actions) => {                  
-                      axios.post('https://immense-scrubland-96694.herokuapp.com/login/signin', values)
-                      .then((response) =>{
-                        console.log(response.data.token);
-                      })
-                      .catch(function (error) {
-                        console.log(error.response);
-                      })
-                      .then(function () {
-                        // always executed
-                      });  
-                    }}
-                    validationSchema={validationSchema}
-                  >
-                    {formikProps => (
-                      <React.Fragment>
-                        <View style={{ marginHorizontal: 20, marginVertical: 5 }}>
-                          <Text style={{ marginBottom: 3 }}>Usuario</Text>
-                          <TextInput
-                            placeholder="johndoe@example.com" name='userName'
-                            style={{
-                              backgroundColor:"rgba(192,192,192,0.3)",
-                              marginBottom: 3,
-                            }}
-                            onChangeText={formikProps.handleChange('userName')}
-                            onBlur={formikProps.handleBlur('userName')}
-                          // autoFocus
-                          />
-                          <Text style={{ color: 'darkred' }}>
-                            {formikProps.touched.userName && formikProps.errors.userName}
-                          </Text>
-                        </View>
+  <View style={styles.container}>
+    
+    <ImageBackground 
+      source={require('../assets/blured.jpg')}
+      style={{ flex: 1,
+        width: null,
+        height: null,
+        }}>
+        <View style={styles.container2}>
+          <Titulo content='Tripo' /> 
+          <Text style={{ marginLeft: 30,fontSize:24,color:"#2a3078" }}>Login</Text>
+          <SafeAreaView style={{ marginTop: 40 }}>
+            <Formik
+              initialValues={{ userName: '', contrasenia: '' }}
+              onSubmit={(values, actions) => {
+                  axios.post('https://immense-scrubland-96694.herokuapp.com/login/signin', values)
+                  .then((response) =>{
+                    console.log(response.data.token);
+                  })
+                  .catch(function (error) {
+                    console.log(error.response);
+                  })
+              }}
+              validationSchema={validationSchema}
+            >
+              {formikProps => (
+                <React.Fragment>
+                  <View style={{ marginHorizontal: 20, marginVertical: 5 }}>
+                    <Text style={{ marginBottom: 3 }}>Email</Text>
+                    <TextInput name = "userName"
+                      placeholder="johndoe@example.com"
+                      style={{
+                        backgroundColor:"rgba(192,192,192,0.3)",
+                        marginBottom: 3,
+                      }}
+                      onChangeText={formikProps.handleChange('userName')}
+                      onBlur={formikProps.handleBlur('userName')}
+                     // autoFocus
+                    />
+                    <Text style={{ color: 'darkred' }}>
+                      {formikProps.touched.email && formikProps.errors.email}
+                    </Text>
+                  </View>
 
-                        <View style={{ marginHorizontal: 20, marginVertical: 5 }}>
-                          <Text style={{ marginBottom: 3 }}>Password</Text>
-                          <TextInput
-                            placeholder="password" name='contrasenia'
-                            style={{
-                              backgroundColor:"rgba(192,192,192,0.3)",
-                              marginBottom: 3,
-                            }}
-                            onChangeText={formikProps.handleChange('contrasenia')}
-                            onBlur={formikProps.handleBlur('contrasenia')}
-                            secureTextEntry
-                          />
-                          <Text style={{ color: 'darkred' }}>
-                            {formikProps.touched.contrasenia && formikProps.errors.contrasenia}
-                          </Text>
-                          <Text                  
-                            style={styles.registerTextStyle}
-                            onPress={this.props.irme}>
-                            ¿No tenés cuenta? Registrate
-                          </Text>
-                        
-                          
-                        </View>
+                  <View style={{ marginHorizontal: 20, marginVertical: 5 }}>
+                    <Text style={{ marginBottom: 3 }}>Password</Text>
+                    <TextInput name="contrasenia"
+                      placeholder="password"
+                      style={{
+                        backgroundColor:"rgba(192,192,192,0.3)",
+                        marginBottom: 3,
+                      }}
+                      onChangeText={formikProps.handleChange('contrasenia')}
+                      onBlur={formikProps.handleBlur('contrasenia')}
+                      secureTextEntry
+                    />
+                    <Text style={{ color: 'darkred' }}>
+                      {formikProps.touched.password && formikProps.errors.password}
+                    </Text>
+                    <Text                  
+                      style={styles.registerTextStyle}
+                      onPress={this.props.irme}>
+                      ¿No tenés cuenta? Registrate
+                    </Text>
+                  
+                    
+                  </View>
 
-                        {formikProps.isSubmitting ? (
-                          <ActivityIndicator />
-                        ) : (
-                          <View style={styles.inputs}>
-                            <Button title="Iniciar Sesión" onPress={formikProps.handleSubmit} />
-                          </View>
-                        )}
-                      </React.Fragment>
-                    )}
-                  </Formik>
+                  {formikProps.isSubmitting ? (
+                    <ActivityIndicator />
+                  ) : (
+                    <View style={styles.inputs}>
+                      <Button title="Iniciar Sesión" onPress={formikProps.handleSubmit} />
+                    </View>
+                  )}
+                </React.Fragment>
+              )}
+            </Formik>
 
-                </SafeAreaView>
-              </View>
-            </AppContext.Provider>
-        </ImageBackground>
-      </View>
-    );
-  }
-}
+          </SafeAreaView>
+        </View>
+    </ImageBackground>
+  </View>
+);
+ }
+ }
 
 const styles = StyleSheet.create({
   
@@ -148,4 +140,4 @@ const styles = StyleSheet.create({
     fontSize: 14,
   },
   
-});
+})
